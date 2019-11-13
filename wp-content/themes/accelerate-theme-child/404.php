f6419f4c4e017b1c812f8bf909c923faf69bf492
Luisa the Librarian <lsabinkildiss@gmail.com>
2019-11-13 16:27:31 -0500

    to resolve problems with about page flexbox css

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * The template for displaying the 404 page
+ * Template Name: Full-width layout
+ * Template Post Type: post, page
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+<div id="primary" class="site-content sidebar">
+<div class="main-content" role="main">
+
+<div class="error-page-image>"
+
+
+<div class="error-page">
+<h2 >Whoops, Took a Wrong Turn...</h2>
+<p>Sorry, this page no longer exists, never existed or has been moved.  We feel like complete jerks for totally misleading you.</p>
+<p>Feel free to take a look around our blog or some of our featured work.</p>
+</div>
+
+
+<!-- our footer section -->
+<?php get_footer(); ?>
